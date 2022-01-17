faa79b22392e817024b215abf67b6037b176e757
sirojiddin-Developer <sirojiddinCoder9998@gmail.com>
2022-01-16 22:46:08 -0800

    full project map

--- props-project/src/App.js
+++ props-project/src/App.js
@@ -4,27 +4,28 @@ import Fruit from './Fruit'
 
 
 const data = [
-  {id:1,img:'./',title:'Apple',uom:'02',size:'02',tit:'$8.99',tota:'$17.88'},
-  {id:2,img:'./',title:'Banana',uom:'02',size:'02',tit:'$8.99',tota:'$17.88'},
-  {id:3,img:'./',title:'Olma',uom:'02',size:'02',tit:'$8.99',tota:'$17.88'},
-  {id:4,img:'./',title:'Gilos',uom:'02',size:'02',tit:'$8.99',tota:'$17.88'},
+  {id:1,img:'https://st.depositphotos.com/1642482/1904/i/600/depositphotos_19049353-stock-photo-fruit.jpg'
+  ,title:'Apple',uom:'02',size:'02',unit:'$8.99',total:'$17.88'},
+  {id:2,img:'https://st.depositphotos.com/1642482/1904/i/600/depositphotos_19049353-stock-photo-fruit.jpg',title:'Banana',uom:'02',size:'02',unit:'$18.99',total:'$17.88'},
+  {id:3,img:'https://st.depositphotos.com/1642482/1904/i/600/depositphotos_19049353-stock-photo-fruit.jpg',title:'Olma',uom:'02',size:'02',unit:'$7.99',total:'$17.88'},
+  {id:4,img:'https://st.depositphotos.com/1642482/1904/i/600/depositphotos_19049353-stock-photo-fruit.jpg',title:'Gilos',uom:'02',size:'02',unit:'$6.99',total:'$17.88'},
+  {id:5,img:'https://st.depositphotos.com/1642482/1904/i/600/depositphotos_19049353-stock-photo-fruit.jpg',title:'Apple',uom:'02',size:'02',unit:'$9.99',total:'$17.88'},
+  {id:6,img:'https://st.depositphotos.com/1642482/1904/i/600/depositphotos_19049353-stock-photo-fruit.jpg',title:'shaftoli',uom:'02',size:'02',unit:'$8.99',total:'$17.88'},
+  {id:7,img:'https://st.depositphotos.com/1642482/1904/i/600/depositphotos_19049353-stock-photo-fruit.jpg',title:'Anor',uom:'02',size:'02',unit:'$8.99',total:'$17.88'},
+  {id:8,img:'https://st.depositphotos.com/1642482/1904/i/600/depositphotos_19049353-stock-photo-fruit.jpg',title:'Ananas',uom:'02',size:'02',unit:'$10.99',total:'$17.88'},
 ]
 function App() {
   return (
     <div className="container">
-     {
-       
-         data.map((value,index)=>{
-           return(
-             
-            <div>
-              <Fruit />
-              </div>
-
-           )
-
-       })
-     }
+              {
+                data.map((value,index)=>{
+                  return(
+                    <div>
+                      <Fruit data={value} />
+                    </div>
+                  )
+                })
+              }
    
     </div>
   );
--- props-project/src/Fruit.js
+++ props-project/src/Fruit.js
@@ -1,21 +1,23 @@
 import React from "react";
 import './Fruit.css';
+import banana from './assets/banana.jpg'
 
-function Fruit(){
+function Fruit(props){
     return(
         <div className="main">
             <div className="rasm">
+                <img src={props.data.img} width="100%" height="100%" />
 
             </div>
             <div className="details">
                 <div className="top">
-                    <h1>Celery Stakl</h1>
+                    <h1>{props.data.title}</h1>
                     <button className="button">ADD</button>
                 </div>
                 <div className="bottom">
                     <div className="data">
                         <p className="title">UOM</p>
-                        <p className="value">02</p>
+                        <p className="value">{props.data.size}</p>
                     </div>
                     <div className="data">
                     <p className="title">Pack Size</p>
@@ -23,11 +25,11 @@ function Fruit(){
                     </div>
                     <div className="data">
                     <p className="title">Per Unit</p>
-                        <p className="value">02</p>
+                        <p className="value">{props.data.unit}</p>
                     </div>
                     <div className="data">
                     <p className="title">Total</p>
-                        <p className="value">02</p>
+                        <p className="value">{props.data.total}</p>
                     </div>
                 </div>
 
